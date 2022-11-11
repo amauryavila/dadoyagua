6250be80e2ed054fa7e53d4e49a1fa3407861ae3
amauryavila <100004825+amauryavila@users.noreply.github.com>
2022-11-11 10:49:29 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,62 @@
+let Valor_dado = 0
+let Temp = 0
+input.onButtonPressed(Button.A, function () {
+    Valor_dado = randint(1, 6)
+    basic.showNumber(Valor_dado)
+    if (Valor_dado == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Valor_dado == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Valor_dado == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Valor_dado == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Valor_dado == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+    Temp = 0
+    if (Valor_dado > 0) {
+        Temp = Temp + Valor_dado
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.showNumber(Temp)
+})
